5ada0492fd6722c77f5b254c5fca23cd6984ac14
Fadexz <37004055+Fadexz@users.noreply.github.com>
2022-01-08 23:37:56 +1000

    Update fullscreen_borderless_window.lua

--- fullscreen_borderless_window.lua
+++ fullscreen_borderless_window.lua
@@ -1,15 +1,9 @@
 --[[
 
-Allows for automatically setting up the window for wider than "16:9" video to be viewed across multiple monitors
-side by side aligned to the left of the first monitor with excess video able to display on the second monitor,
-useful if you want a larger video and don't mind bezels.
-You can imagine this as displaying a video across multiple monitors but instead the video will be aligned to the
-left (with no added "black bars"), and on multiple monitors with ease.
+Allows for automatically setting up the window for wider than "16:9" video to be viewed across multiple monitors side by side aligned to the left of the first monitor with excess video able to display on the second monitor, useful if you want a larger video and don't mind bezels.
+You can imagine this as displaying a video across multiple monitors but instead the video will be aligned to the left (with no added "black bars"), and on multiple monitors with ease.
 
-Note: The window border will be disabled and the window will be positioned in the top-left corner and at 100%
-width of the screen. Due to probable mpv limitations, the window is not able to be set wider than the current
-screen so you will need to drag the window larger manually until it fits the full height of the screens.
-The script will still help you automate the rest of the setup.
+Note: The window border will be disabled and the window will be positioned in the top-left corner and at 100% width of the screen. Due to probable mpv limitations, the window is not able to be set wider than the current screen so you will need to drag the window larger manually until it fits the full height of the screens. The script will still help you automate the rest of the setup.
 
 ]]--
 
